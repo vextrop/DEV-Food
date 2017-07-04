6bc9b90fe6532c971ac04e0bd4572bc2a359e9d5
Eduardo555 <eduardospillereanzolin@gmail.com>
2017-07-04 13:54:54 -0300

    Adicionado arquivos de conexao com o banco de dados

--- rundb.sh
+++ rundb.sh
@@ -1,1 +1,1 @@
- java -cp lib/hsqldb.jar org.hsqldb.Server -database.0 file:db/foodb -dbname.0 foodb
+ java -cp lib/hsqldb.jar org.hsqldb.Server -database.0 file:db\foodb -dbname.0 foodb
---
+++ src/com/dev/devfood/dao/conexao.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dev.devfood.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class conexao {
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("org.hsqldb.jdbcDriver");
+            //conn = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/bancodb", "sa", "");
+            conn = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/foodb", "sa", "");
+        } catch (SQLException e) {
+            System.out.println("Problemas ao conectar no banco de dados");
+        } catch (ClassNotFoundException e) {
+            System.out.println("O driver não foi configurado corretametne");
+        }
+
+        return conn;
+    }
+}
